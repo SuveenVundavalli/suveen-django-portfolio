5b0d1ed224782160e1edd42904e37082bdc12230
Suveen Kumar Vundavalli <suveenkumar.vundavalli@gmail.com>
2018-03-27 17:03:53 +0530

    Added favicon and modified static files path

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,11 +31,13 @@ if PRODUCTION:
     SITE_URL = 'www.suveen.me'
     DEBUG = False
     STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+    STATIC_URL = '/static/'
 else:
     ALLOWED_HOSTS = ['localhost']
     SITE_URL = 'localhost:8000'
     DEBUG = True
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles/'), ]
+    STATIC_URL = '/staticfiles/'
 
 # Application definition
 
@@ -126,6 +128,3 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
-
