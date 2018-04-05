120f9f4a0bddf59806694564bcdf3870fe062b83
Suveen Kumar Vundavalli <suveenkumar.vundavalli@gmail.com>
2018-04-05 20:06:09 +0530

    Completed Skills section

--- staticfiles/js/newDesign.js
+++ staticfiles/js/newDesign.js
@@ -2,4 +2,42 @@ $(document).ready(main);
 
 function main() {
 // alert("Welcome to suveen.me")
+
+    /*
+    ==============================================================================
+                                    Sticky Navbar
+    ==============================================================================
+     */
+    $('#nav').affix({
+        offset: {
+            top: $('header').height()
+        }
+    });
+
+    $("#nav").wrap("<div class='nav-placeholder'></div>");
+    $(".nav-placeholder").height($("#nav").outerHeight());
+
+    /*
+    ==============================================================================
+                                Skills Progress Bar
+    ==============================================================================
+     */
+    $(document).on('scroll', function () {
+        console.log($('html,body').scrollTop())
+        console.log(($('header').outerHeight() - 10))
+        if ($('html,body').scrollTop() > ($('header').outerHeight() - 10)) {
+            $('.progress .progress-bar').css("width",
+                function () {
+                    return $(this).attr("aria-valuenow") + "%";
+                }
+            )
+        }
+    });
+
+
+    /*
+    ==============================================================================
+                                Ending tag for main()
+    ==============================================================================
+     */
 }
