7c8e1870ef77a665fcbd398d01daecf5f7c95355
Suveen Vundavalli <suveenkumar.vundavalli@gmail.com>
2019-06-10 21:37:42 +0200

    Added phone number validation

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -98,6 +98,14 @@ function main() {
                     }
                 }
             },
+            mobile: {
+                validators: {
+                    regexp: {
+                        regexp: /^(([+][(]?[0-9]{1,3}[)]?)|([(]?[0-9]{4}[)]?))\s*[)]?[-\s\.]?[(]?[0-9]{1,3}[)]?([-\s\.]?[0-9]{3})([-\s\.]?[0-9]{3,4})$/,
+                        message: "Enter a valid phone number"
+                    }
+                }
+            },
             subject: {
                 validators: {
                     notEmpty: {
